8e68c8278ff872a2a791edddaaa65687cd64c196
Darren Moore <darrenmoore.web@gmail.com>
2016-07-05 16:06:19 -0700

    adding tree click code.  this is not complete yet

--- Nucleus/Agent.cs
+++ Nucleus/Agent.cs
@@ -45,7 +45,7 @@ namespace Nucleus
                         _agentResponse.Add(item);
                         continue;
                     }
-                    if(item.Contains("{ Header = Item Level 1 }"))
+                    if(item.Contains("{ Header = Item Level 1 }") || item.Contains("{ Header = Item Level 2 }"))
                     {
                         _agentResponse.Add(item);
                     }                   
@@ -115,7 +115,7 @@ namespace Nucleus
                                 
                 _connStr = ConfigurationManager.ConnectionStrings["SYSPRO_SQL_SERVER"].ConnectionString;                                
                 DBOpenConnection();
-                _sqlCommand = new SqlCommand("usp_ProjectHeader_ZccPcmPpcmcZcbc", _sqlConn);
+                _sqlCommand = new SqlCommand("usp_ProjectHeader_ZccPcmPpcmcCeZcbc", _sqlConn); //usp_ProjectHeader_ZccPcmPpcmcZcbc
                 _sqlCommand.CommandType = CommandType.StoredProcedure;
                 _sqlCommand.Parameters.Add(new SqlParameter("@Contract", ID));
                 sqlReader = _sqlCommand.ExecuteReader();
@@ -125,7 +125,8 @@ namespace Nucleus
                     while (sqlReader.Read())
                     {
                         AddzContractContactsLineItem(sqlReader.GetString(2) + " " + "{ Header = Item Level 0 }");
-                        AddzContractContactsLineItem(sqlReader.GetString(3) + " " + "{ Header = Item Level 1 }");                      
+                        AddzContractContactsLineItem(sqlReader.GetString(3) + " " + "{ Header = Item Level 1 }");
+                        AddzContractContactsLineItem(sqlReader.GetString(4) + " " + "{ Header = Item Level 2 }");
                         //Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7},\t{8},\t{9},\t{10}", sqlReader.GetString(0), sqlReader.GetGuid(1), sqlReader.GetString(2), sqlReader.GetString(3), sqlReader.GetString(4), sqlReader.GetString(5), sqlReader.GetString(6), sqlReader.GetString(7), sqlReader.GetString(8), sqlReader.GetDateTime(9), sqlReader.GetDateTime(10));
                     }
                 }
